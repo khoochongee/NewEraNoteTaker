3f4b7dc93a48c843c64f86099d46b7b175aa47ab
khoo <khoochongee1255@e.newera.edu.my>
2019-10-05 12:36:24 +0800

    check username and password, and toast the result

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView edtUsername,edtPassword;
+    Button btnLogin;
+    private String username= "Khoo Chong Ee";
+    private String password= "khoochongee1255";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        setListeners();
+    }
+
+    private void findViews(){
+        edtUsername=findViewById(R.id.user_input);
+        edtPassword=findViewById(R.id.password_input);
+        btnLogin=findViewById(R.id.btn_login);
+    }
+
+    private void setListeners(){
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputUsername=edtUsername.getText().toString();
+                String inputPassword=edtPassword.getText().toString();
+
+                if(inputUsername.equals(username)&& inputPassword.equals(password)){
+                    Toast.makeText(MainActivity.this,"Login Sucessful",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"Login Fail",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
